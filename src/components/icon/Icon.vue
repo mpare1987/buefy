f14fbf5b8dcab6fc479d2b111f83a7f472d89eb6
Mike Pare <MPare@corp.certify.com>
2020-02-22 05:34:45 -0500

    Modify buefy icon component to use Google material icons

--- src/components/icon/Icon.vue
+++ src/components/icon/Icon.vue
@@ -2,14 +2,9 @@
     <span class="icon" :class="[newType, size]">
         <i
             v-if="!useIconComponent"
-            :class="[newPack, newIcon, newCustomSize, customClass]"/>
-
-        <component
-            v-else
-            :is="useIconComponent"
-            :icon="[newPack, newIcon]"
-            :size="newCustomSize"
-            :class="[customClass]"/>
+            :class="['material-icons']"/>
+            {{icon}}
+        >
     </span>
 </template>
 
@@ -20,26 +15,14 @@ import getIcons from '../../utils/icons'
 export default {
     name: 'BIcon',
     props: {
+        icon: {
+            type:String,
+            required:true
+        },
         type: [String, Object],
-        component: String,
-        pack: String,
-        icon: String,
         size: String,
-        customSize: String,
-        customClass: String,
-        both: Boolean // This is used internally to show both MDI and FA icon
     },
     computed: {
-        iconConfig() {
-            let allIcons = getIcons()
-            return allIcons[this.newPack]
-        },
-        iconPrefix() {
-            if (this.iconConfig && this.iconConfig.iconPrefix) {
-                return this.iconConfig.iconPrefix
-            }
-            return ''
-        },
         /**
         * Internal icon name based on the pack.
         * If pack is 'fa', gets the equivalent FA icon name of the MDI,
@@ -48,9 +31,7 @@ export default {
         newIcon() {
             return `${this.iconPrefix}${this.getEquivalentIconOf(this.icon)}`
         },
-        newPack() {
-            return this.pack || config.defaultIconPack
-        },
+
         newType() {
             if (!this.type) return
 
@@ -72,37 +53,8 @@ export default {
         newCustomSize() {
             return this.customSize || this.customSizeByPack
         },
-        customSizeByPack() {
-            if (this.iconConfig && this.iconConfig.sizes) {
-                if (this.size && this.iconConfig.sizes[this.size] !== undefined) {
-                    return this.iconConfig.sizes[this.size]
-                } else if (this.iconConfig.sizes.default) {
-                    return this.iconConfig.sizes.default
-                }
-            }
-            return null
-        },
-        useIconComponent() {
-            return this.component || config.defaultIconComponent
-        }
     },
     methods: {
-        /**
-        * Equivalent icon name of the MDI.
-        */
-        getEquivalentIconOf(value) {
-            // Only transform the class if the both prop is set to true
-            if (!this.both) {
-                return value
-            }
-
-            if (this.iconConfig &&
-                this.iconConfig.internalIcons &&
-                this.iconConfig.internalIcons[value]) {
-                return this.iconConfig.internalIcons[value]
-            }
-            return value
-        }
     }
 }
 </script>
